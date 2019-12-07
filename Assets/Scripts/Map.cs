4141ec4aef11eb94dc85156e287aab5ef49291ab
rrhaenys <123yk123@mail.ru>
2019-12-07 15:35:45 +0300

    first commit

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,49 @@
+﻿using SimpleJSON;
+using System;
+
+[Serializable]
+public class Map
+{
+	public int map_size;
+	public int map_count;
+	public int[,,] map;
+
+	public Map(string savedData)
+	{
+		Load(savedData);
+	}
+
+	private int Load(string savedData)
+	{
+		var N = JSON.Parse(savedData);
+		if (N["map_size"] != null)
+			map_size = N["map_size"].AsInt;
+		else
+			return ErrorMap();
+		if (N["map_count"] != null)
+			map_count = N["map_count"].AsInt;
+		else
+			return ErrorMap();
+		map = new int[map_count, map_size, map_size];
+		for (int i = 0; i < map_count; i++)
+		{
+			for (int j = 0; j < map_size; j++)
+			{
+				for (int k = 0; k < map_size; k++)
+				{
+					if (!(N["map"][i][j][k] != null &&
+						int.TryParse(N["map"][i][j][k].Value, out map[i, j, k])))
+						return ErrorMap();
+				}
+			}
+		}
+		return (0);
+	}
+
+	private int ErrorMap()
+	{
+		map_size = 0;
+		map_count = 0;
+		return (1);
+	}
+}
---
+++ Assets/Scripts/RespawnBlock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnBlock : MonoBehaviour
+{
+	public GameObject prefab;
+	public float step = 0.15f;
+
+	private List<Block> blocks = new List<Block>();
+
+	private void Awake()
+	{
+		Get_Map_json.Get_map += Respawn_blocks;
+	}
+
+	private void Respawn_blocks(Map map)
+	{
+		Debug.Log("Respawn_blocks");
+		while (blocks.Count > 0)
+			Destroy(blocks[0].gameObject);
+		for (int i = 0; i < map.map_size; i++)
+		{
+			for (int j = 0; j < map.map_size; j++)
+			{
+				GameObject cube = Instantiate(prefab,
+												new Vector3(i * step, 0, j * step),
+												Quaternion.identity, transform);
+				Block block = cube.AddComponent<Block>();
+				block.x = i;
+				block.y = j;
+				block.num = map.map[0, i, j];
+				blocks.Add(block);
+			}
+		}
+	}
+}
